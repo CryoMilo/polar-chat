80884ebad8ce30255e6a656ed18bda46c0c29cae
Damian Aung <minkojee@gmail.com>
2026-07-31 23:46:29 +0700

    added notify online backend

--- backend/socket.js
+++ backend/socket.js
@@ -1,9 +1,19 @@
+import { leaveAllRooms } from "./socket/helpers.js";
+import { notifyConversationOnlineStatus } from "./socket/socketConversation.js";
+
 export const initializeSocket = async (io) => {
 	io.on("connection", async (socket) => {
 		try {
 			const user = socket.user;
 			console.log("User connected", user.id);
 			socket.join(user._id.toString());
+
+			await notifyConversationOnlineStatus(io, socket, true);
+
+			socket.on("disconnect", async () => {
+				await notifyConversationOnlineStatus(io, socket, false);
+				leaveAllRooms(socket);
+			});
 		} catch (error) {
 			console.error("Socket Connection Error", error);
 			socket.emit("internal_error", { error: "Internal Server Error" });
